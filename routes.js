b636611e3b77bf5fb2c2c2a8362e93ce7895268e
Chad Johnston <cjohnston@megatome.com>
2013-08-15 15:26:05 -0600

    Change route behavior.

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 exports = module.exports = function(app, passport) {
   //Pull status
-  app.get('/:userid/:repo/:id', require('./views/status/index').init);
+  app.get('/:userid/:repo/:id', require('./views/status/index').pullStatus);
   
   //route not found
   app.all('*', require('./views/http/index').http404);
