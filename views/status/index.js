b3e66faa733b02266ef65b585604aa1a9568f323
Chad Johnston <cjohnston@megatome.com>
2013-08-16 11:39:41 -0600

    Change out Github module.
    Github API seems to have a defect when querying for merge status. I
    thought it was a library issue, so I changed. I like the new lib better,
    so I'm keeping it.

--- views/status/index.js
+++ views/status/index.js
@@ -1,3 +1,35 @@
 exports.pullStatus = function(req, res){
-    res.send("Pull Status");
+
+    var GitHubApi = require("github");
+
+    var github = new GitHubApi({
+        version: "3.0.0"
+    });
+    github.debug = true;
+
+    github.pullRequests.getMerged({user: req.params.userid, repo: req.params.repo, number: req.params.id}, function (err, body) {
+        console.log(err);
+        console.log(body);
+        if (err) {
+            if (err.code == 404) {
+                res.sendfile('public/media/not_merged.png');
+            }
+            else {
+                res.send(404, 'Ooops');
+            }
+        }
+        else {
+            res.send("Something");
+        }
+    });
+    //client.get('/repos/' + req.params.userid + '/' + req.params.repo + '/pulls/' + req.params.id + '/merge', function (err, status, body) {
+    /*client.get('/users/iamthechad', function (err, status, body) {
+        console.log(status);
+        if (err) {
+            console.log(err);
+            res.send(404, "No information found for that request")
+        }
+        res.send(body); //json object
+    });*/
+    //res.send('/repos/' + req.params.userid + '/' + req.params.repo + '/pulls/' + req.params.id + '/merge');
 };
